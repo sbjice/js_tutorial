536859adf0b87885c8f7c191bbd7ffce41080aa9
sbjice <boy-ram@mail.ru>
2021-06-04 14:26:40 +0300

    operator priority and type casting learnt

--- first_project/js/script.js
+++ first_project/js/script.js
@@ -295,3 +295,108 @@ const soldier = {
 
 const john = Object.create(soldier);
 */
+
+
+
+// Dynamical typing
+/*
+// To String
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+// 2)
+console.log(typeof(null + ""));
+console.log("url/"+5);
+
+// To Number
+// 1)
+console.log(typeof(Number('4')));
+// 2)
+console.log(typeof(+'4'));
+// 3)
+console.log(typeof(parseInt('15px', 10))); //10 - numeral system
+
+// To Boolean
+// 1)
+let switcher = null;
+if (switcher) {
+    console.log("working");
+}
+
+switcher = 0;
+if (switcher) {
+    console.log("working");
+}
+
+switcher = 1;
+if (switcher) {
+    console.log("working");
+}
+// 2)
+console.log(typeof(Boolean('4')));
+console.log(typeof(Boolean(4)));
+console.log(typeof(Boolean(0)));
+console.log(typeof(Boolean('')));
+console.log(typeof(Boolean(null)));
+console.log(typeof(Boolean(undefined)));
+
+console.log(Boolean('4')); // true
+console.log(Boolean(4)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean('')); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(false)); // false
+console.log(Boolean(true)); // true
+// 3)
+console.log(typeof(!!(undefined)));
+
+*/
+
+
+// interview questions
+// operator priority
+/*
+// 1) 
+let x = 5;
+alert(x++); // 5
+// 2)
+[] + false - null + true;
+// "-" makes interpretator use expression as mathematical, if any of operands in expression gives non-numeric value, the expression will be NaN
+console.log(typeof ([] + false - null + true)); // number
+console.log(typeof ([] + false - true)); // number
+console.log(typeof ([] + false - null)); // number, NaN,  
+console.log(typeof ([] + false)); // string, concatenation
+console.log(+[]);
+// 3)
+let y = 1;
+let c = y = 2;
+alert(c); // 2, multiple assignment
+// 4)
+[] + 1 + 2; // 12, [] turns into "", then goes concatenation
+// 5)
+alert("1" [0]); // 1, string slicing
+// 6)
+2 && 1 && null && 0 && undefined; // null, 
+// operator && stops on false, returns first found falsy operand
+// operator || stops on True, returns last found falsy operand
+// && returns last truthy operand
+// || returns first truthy operand
+// 7)
+!!( a && b ) === (a && b); // false 
+// 8)
+console.log( null || 2 && 3 || 4 ); // 3, && has fisrt priority, || has second priority
+// 9)
+let a = [1, 2, 3],
+    b = [1, 2, 3];
+console.log(a == b); // false, different containers
+console.log([1, 2, 3] == [1, 2, 3]);  // false, different containers
+// 10)
+console.log( +"Infinity" ); // Infinity, type - number, obvious
+console.log(typeof( +"Infinity" ));
+// 11)
+console.log("Ёжик" > "яблоко"); //false, lexicographical comparison
+// 12)
+console.log(0 || "" || 2 || undefined || true || falsе ); //2 , first truthy
+
+*/
